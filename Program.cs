fe6ed8396182b2a1bad85a09d08a2c51459513d7
izafa-tagiyeva <izafact-bp215@code.edu.ez>
2024-10-19 00:02:56 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using ConsoleApp1;
+
+
+
+
+/*  Weapon deyə class yaradılır.
+Darağın güllə tutumu, Daraqdakı güllə sayı,atış modu(təkli və ya avtomatik) propertileri verilir.
+Shoot() -  metodu hər çağırıldıqda fire moduna gore ya 1 güllə atır yada avtomatik gulleni bosaldir.(Ekranda bunu bildirir)
+GetRemainBulletCount() - darağın tam dolması üçün lazım olan güllə sayını qaytarır.
+Reload() - darağı yenidən doldurur.
+ChangeFireMode() - Atış modunu dəyişir.
+Bütün məlumatları doldurmadan obyekt yaratmaq olmasın.
+0 - İnformasiya almaq üçün
+1 - Shoot metodu üçün
+2 - GetRemainBulletCount metodu üçün
+3 - Reload metodu üçün
+4 - ChangeFireMode metodu üçün
+5 - Proqramdan dayandırmaq üçün qısayoldur.*/
+
+
+
+
+
+
+            Weapon new_gun = new Weapon();
+            Console.WriteLine("tutumu daxil edin");
+            int tutum = int.Parse(Console.ReadLine());
+            Console.WriteLine("say daxil edin");
+            int say = int.Parse(Console.ReadLine());
+            Console.WriteLine("atis modun teyin edin.tekli modu ucun true yazsin daxil edin,umumi atim ucun false yazisin daxil edin");
+            bool atis_modu = bool.Parse(Console.ReadLine());
+            new_gun = new Weapon(tutum, say, atis_modu);
+
+
+
+
+int choiche;
+do
+{
+    Console.WriteLine("0 - İnformasiya almaq üçün\r\n 1 - Shoot metodu üçün\r\n  2 - GetRemainBulletCount metodu üçün\r\n 3 - Reload metodu üçün\r\n   4 - ChangeFireMode metodu üçün\r\n 5 - Proqramdan dayandırmaq üçün");
+    choiche = int.Parse(Console.ReadLine());
+
+
+    
+    switch (choiche)
+    {
+        case 0:
+            new_gun.GetInfo(tutum,say,atis_modu);
+            break;
+        case 1:
+            new_gun.Shoot(new_gun.atis_modu);
+            break;
+        case 2:
+            Console.WriteLine($"Daragin tam dolmasi ucun lazim olan gulle sayi:{new_gun.GetRemainBulletCount(new_gun.say, new_gun.tutum)}");
+            break;
+        case 3:
+            new_gun.say = new_gun.Reload();
+            Console.WriteLine("darağı yenidən dolduruldu");
+            break;
+        case 4:
+            new_gun.atis_modu = new_gun.ChangeFireMode(new_gun.atis_modu);
+            Console.WriteLine("atis modu deyisdirildi");
+            break;
+        case 5:
+            Console.WriteLine("Exiting..");
+            break;
+
+        default:
+            Console.WriteLine("invalid option");
+            break;
+
+    }
+} while (choiche != 5);
---
+++ Weapon.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Weapon
+    {
+
+        public bool atis_modu;
+        private int _tutum;
+        public int tutum
+        {
+            get { return _tutum; }
+            set
+            {
+
+                if (value > 0)
+                {
+                    _tutum = value;
+                }
+                else { Console.WriteLine("please make sure that number is greater than zero"); }
+
+            }
+        }
+
+
+        private int _say;
+        public int say
+        {
+            get { return _say; }
+            set
+            {
+                if (value > 0)
+                {
+                    _say = value;
+                }
+                else { Console.WriteLine("please make sure that number is greater than zero"); }
+            }
+        }
+        public Weapon()
+        {
+
+        }
+
+        public Weapon(int tutum, int say, bool atis_modu)
+        {
+
+            this._tutum = tutum;
+            this._say = say;
+            this.atis_modu = atis_modu;
+        }
+
+
+
+
+        //informasi alamq ucun metod
+        public void GetInfo(int tutum, int say, bool atis_modu)
+        {
+            Console.WriteLine($"gullenin tutumu:{tutum}, sayi:{say}");
+            if (atis_modu)
+            {
+                Console.WriteLine("atis modu: tekli");
+            }
+            else { Console.WriteLine("atis modu: umumi"); }
+        }
+
+
+
+
+        //Shoot() -  metodu hər çağırıldıqda fire moduna gore ya 1 güllə atır yada avtomatik gulleni bosaldir.(Ekranda bunu bildirir)
+
+        public void Shoot(bool atis_modu)
+        {
+            if (atis_modu)
+            {
+                _say = _say - 1;
+                Console.WriteLine("1 gulle azaldi");
+            }
+            else
+            {
+                _say = 0;
+                Console.WriteLine("hamisi bitdi");
+            }
+        }
+
+        //GetRemainBulletCount() - darağın tam dolması üçün lazım olan güllə sayını qaytarır.
+
+        public int GetRemainBulletCount(int _say, int _tutum)
+        {
+            int lazim_olan = _tutum - _say;
+            return lazim_olan;
+
+        }
+        // Reload() - darağı yenidən doldurur.
+        public int Reload()
+        {
+            _say = _tutum;
+            return _say;
+        }
+
+        //ChangeFireMode() - Atış modunu dəyişir.
+        public bool ChangeFireMode(bool atis_modu)
+        {
+            if (atis_modu)
+            {
+                return atis_modu = false;
+            }
+            else { return atis_modu = true; }
+        }
+
+
+    }
+}
